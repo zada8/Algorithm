495d5cdcd0bbcdba0bec20bcf7dd6d96cba10b60
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-07-18 20:28:53 +0900

    [Bronze V] Title: ??!, Time: 112 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String id = br.readLine();
+		
+		System.out.println(id+"??!");
+	}
+
+}
