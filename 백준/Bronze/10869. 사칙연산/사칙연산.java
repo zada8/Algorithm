4216b70c4df5310c2eaeb0a324a89259302fd768
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-07-18 20:21:55 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int add = a+b;
+		int sub = a-b;
+		int mul = a*b;
+		int div = a/b;
+		int mod = a%b;
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
+		System.out.println(mod);
+	}
+
+}
