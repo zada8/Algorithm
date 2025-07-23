b705b9bfa5ca7f802b25fb4e3042ec150de49a3a
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-07-24 03:14:53 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 85424 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws  IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = 666;
+		int cnt = 1;
+		
+		while(cnt < N) {
+			num++;
+			if (String.valueOf(num).contains("666")) {
+				cnt++; //666포함
+			}
+		}
+		System.out.println(num);
+
+	}
+
+}
