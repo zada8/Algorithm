0d7d7b4e7bfa2c9eff134daeefc9bf0ff0d21cdc
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-08-04 00:38:39 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14684 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		for(int i = 2; i <= N; i++) {
+			while(N%i == 0) {
+				System.out.println(i);
+				N = N/i;
+			}
+		}
+		
+		
+	}
+	
+
+
+}
