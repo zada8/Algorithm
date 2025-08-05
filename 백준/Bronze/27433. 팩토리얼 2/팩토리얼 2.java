2d58f216038d3b9a5b16acda598ffa16cb9705d3
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-08-06 01:38:38 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	
+	private static int fac;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(facto(N));
+		
+	}
+
+	public static long facto(int i) {
+		if(i==0) {
+			return 1;
+		}
+		
+		return  facto(i-1)*i;
+	}
+
+}
