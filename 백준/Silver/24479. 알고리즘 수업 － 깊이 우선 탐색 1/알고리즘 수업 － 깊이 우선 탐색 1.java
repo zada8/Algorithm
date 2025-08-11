3f718e5d1ece4636c03997531b2e6e70095949d2
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-08-12 00:12:30 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1500 ms, Memory: 97072 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -8,24 +8,26 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	private static int[] visited;
-	private static int N;
+	
+	
 	private static ArrayList<Integer>[] graph;
+	private static int[] visited;
 	private static int cnt;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); //정점
+		int N = Integer.parseInt(st.nextToken()); //정점의 수
 		int M = Integer.parseInt(st.nextToken()); //간선의 수
 		int R = Integer.parseInt(st.nextToken()); //시작 정점
 		visited = new int[N];
 		cnt = 1;
 		
-		graph = new ArrayList[N]; //인접리스트 선언
+		graph = new ArrayList[N];
 		for(int i = 0; i<N; i++) {
-			graph[i] = new ArrayList<Integer>(); //객체 생성
+			graph[i] = new ArrayList<Integer>();
 		}
+		//객체 생성
 		
 		for(int i = 0; i<M; i++) {
 			StringTokenizer st2 = new StringTokenizer(br.readLine());
@@ -36,28 +38,28 @@ public class Main {
 		}
 		///그래프 완성
 		
-		for(int i =0; i<N; i++) {
+		for(int i = 0; i<N; i++) {
 			if(graph[i].size()!=0) {
 				Collections.sort(graph[i]);
 			}
 		}
-		///그래프 정렬
 		
 		dfs(R-1);
-		for(int i = 0; i< N; i++) {
+		
+		for(int i = 0; i<N; i++) {
 			System.out.println(visited[i]);
 		}
-			
 	}
 	
 	public static void dfs(int idx) {
 		visited[idx] = cnt++;
 		
-		for(int i = 0; i<graph[idx].size(); i++) {
-			if(visited[graph[idx].get(i)] == 0) {
-				dfs(graph[idx].get(i));
+		for(int i:graph[idx]) {
+			if(visited[i] == 0) {
+				dfs(i);
 			}
 		}
+		
 	}
 
 }
