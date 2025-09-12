6f36ec1165ace93569e864ecd1cad883ef855888
Hanbyeol Kang <45020613+zada8@users.noreply.github.com>
2025-09-12 10:41:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		if(n == 1) {
+			System.out.println(1);
+			return;
+		}
+		if(n ==2) {
+			System.out.println(2);
+			return;
+		}
+		
+		int[] dp = new int[n+1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i = 3; i<=n; i++) {
+			dp[i] = (dp[i-2]+dp[i-1])%10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+
+}
